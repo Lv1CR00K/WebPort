01f26df72c3f78b49e389a373808f67bca218b9e
Lv1CR00K <brainvirus990@gmail.com>
2025-02-18 13:28:51 +0800

    Updated No PHP

--- head.php
+++
@@ -1,6 +0,0 @@
-<head>
-    <title>Pakingan - Portfolio</title>
-    <link rel="icon" href="public/crookwork.ico" type="image/x-icon">
-    <link rel="stylesheet" href="VelOne/crookwork.css">
-    <link rel="stylesheet" href="style/root.css">
-</head>
